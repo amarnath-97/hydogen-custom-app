ac4074d45e232334b18ef91f7ae6cbf818bca241
F22 Labs <amarnath@f22labs.com>
2023-01-25 11:40:03 +0530

    Latest commit with some style changes

--- src/routes/index.server.tsx
+++ src/routes/index.server.tsx
@@ -22,12 +22,6 @@ const Home = () => {
       <Layout>
         <div className="banner w-full h-full relative">
           <Banner />
-          <div className="absolute text-white top-[45%] left-[50%] translate-x-[-50%] translate-y-[-50%]  bg-black bg-opacity-[.4] p-5">
-            <h1 className="sm:text-lg md:text-2xl lg:text-6xl text-center font-semibold mb-5">
-              Welcome to Urban Fashion
-            </h1>
-            <p className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis fugiat id voluptatem optio voluptates aspernatur velit tempora delectus architecto tempore.</p>
-          </div>
         </div>
         <div className="collection_heading w-full h-12 border border-black flex items-center justify-center font-semibold text-xl">
           Collections
